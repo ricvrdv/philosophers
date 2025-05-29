e820b1de2aa94075fabc1f029387b05e5d1abf36
ricvrdv <costagarcia.r95@gmail.com>
2025-05-29 15:09:09 +0100

    Norminette fixes

--- philo/main.c
+++ philo/main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 15:00:52 by rjesus-d          #+#    #+#             */
+/*   Updated: 2025/05/29 15:00:57 by rjesus-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
 // ./philo 1 2 3 4 [5]
@@ -7,25 +19,23 @@
 // 4. time_to_sleep (in ms)
 // 5. number_of_times_each_philosopher_must_eat
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-  t_table table;
+	t_table	table;
 
-  if (argc == 5 || argc == 6)
-  {
-    if (parse_args(&table, argv) == -1)
-    {
-      print_error("Arguments must be 0 or positive numbers\n");
-      return (1);
-    }
-    if (init_structs(&table) == -1)
-      return (1);
-    //printf("argv 1: %ld\nargv 2: %ld\nargv 3: %ld\nargv 4: %ld\nargv 5: %ld\n", table.nbr_philos, table.time_to_die, table.time_to_eat, table.time_to_sleep, table.nbr_limit_meals);
-    if (start_simulation(&table) == -1)
-      return (1);
-    // FREE MEMORY      TODO
-  }
-  else
-  	print_error("Run the program with 4 or 5 arguments\n");
-  return (0);
+	if (argc == 5 || argc == 6)
+	{
+		if (parse_args(&table, argv) == -1)
+		{
+			print_error("Arguments must be 0 or positive numbers\n");
+			return (1);
+		}
+		if (init_structs(&table) == -1)
+			return (1);
+		if (start_simulation(&table) == -1)
+			return (1);
+	}
+	else
+		print_error("Run the program with 4 or 5 arguments\n");
+	return (0);
 }
--- philo/utils.c
+++ philo/utils.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/29 15:01:13 by rjesus-d          #+#    #+#             */
+/*   Updated: 2025/05/29 15:04:20 by rjesus-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
 // print_error()
 // ft_atol()
 
-long  get_time_in_ms(void)
+long	get_time_in_ms(void)
 {
-  struct timeval	tv;
-  
-  if (gettimeofday(&tv, NULL) == -1)
-    return (-1); 
-  return (tv.tv_sec * 1000L + tv.tv_usec / 1000);
+	struct timeval	tv;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		return (-1);
+	return (tv.tv_sec * 1000L + tv.tv_usec / 1000);
 }
 
 void	print_error(const char *message)
@@ -44,18 +56,18 @@ static long	convert_digits(const char *str)
 	return (result);
 }
 
-long    ft_atol(const char *str)
+long	ft_atol(const char *str)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    if (*str == '\0')
-        return (-1);
-    while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
-        i++;
-    if (str[i] == '+')
+	i = 0;
+	if (*str == '\0')
+		return (-1);
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+')
 		i++;
 	else if (str[i] == '-')
 		return (-1);
-    return (convert_digits(str + i));
+	return (convert_digits(str + i));
 }
