0d05661b84be4a3f77b7edc518a71598444ce12b
ricvrdv <costagarcia.r95@gmail.com>
2025-07-04 20:12:54 +0100

    Modified colors for the output

--- philo/write.c
+++ philo/write.c
@@ -6,7 +6,7 @@
 /*   By: rjesus-d <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 20:03:11 by rjesus-d          #+#    #+#             */
-/*   Updated: 2025/07/04 19:46:15 by rjesus-d         ###   ########.fr       */
+/*   Updated: 2025/07/04 20:12:07 by rjesus-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ void	write_status(t_status status, t_philo *philo)
 		&& !dinner_ended)
 		printf("%ld\t%d has taken a fork\n", timestamp, philo->id);
 	else if (status == EATING && !dinner_ended)
-		printf(G"%ld\t%d"DEF" is "G"eating\n"DEF, timestamp, philo->id);
+		printf(G"%ld\t%d"DEF" is "G"eating"DEF"\n", timestamp, philo->id);
 	else if (status == SLEEPING && !dinner_ended)
-		printf("%ld\t%d is "P"sleeping\n"DEF, timestamp, philo->id);
+		printf("%ld\t%d is "P"sleeping"DEF"\n", timestamp, philo->id);
 	else if (status == THINKING && !dinner_ended)
-		printf("%ld\t%d is "C"thinking\n"DEF, timestamp, philo->id);
+		printf("%ld\t%d is "C"thinking"DEF"\n", timestamp, philo->id);
 	else if (status == DIED)
-		printf(R"%ld\t%d died\n"DEF, timestamp, philo->id);
+		printf(R"%ld\t%d died"DEF"\n", timestamp, philo->id);
 	pthread_mutex_unlock(&philo->table->write_mutex);
 }
